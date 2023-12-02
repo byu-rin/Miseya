25ea4b6d0c025999ac82405b7d46b7d24078b365
Lee do yeon <simon3397@gmail.com>
2023-12-03 01:06:52 +0900

    [BUG_FIX] JSON 데이터 형식과 DUST 클래스 구조 일치하도록 수정

--- app/src/main/java/com/example/miseya/data/DustDTO.kt
+++ app/src/main/java/com/example/miseya/data/DustDTO.kt
@@ -1,7 +1,12 @@
 import com.google.gson.annotations.SerializedName
 
-data class Dust(val response: DustResponse)
+// 대기 오염 정보를 담는 최상위 데이터 클래스
+data class Dust(
+    @SerializedName("response")
+    val response: DustResponse
+)
 
+// 대기 오염 정보 응답에 대한 데이터 클래스
 data class DustResponse(
     @SerializedName("body")
     val dustBody: DustBody,
@@ -9,19 +14,25 @@ data class DustResponse(
     val dustHeader: DustHeader
 )
 
+// 대기 오염 정보 응답에서 Body에 대한 데이터 클래스
 data class DustBody(
+    @SerializedName("totalCount")
     val totalCount: Int,
     @SerializedName("items")
-    val dustItem: MutableList<DustItem>?,
+    val dustItem: MutableList<DustItem>?, // 대기 오염 항목들을 저장하는 리스트
+    @SerializedName("pageNo")
     val pageNo: Int,
+    @SerializedName("numOfRows")
     val numOfRows: Int
 )
 
+// 대기 오염 정보 응답에서 Header에 대한 데이터 클래스
 data class DustHeader(
     val resultCode: String,
     val resultMsg: String
 )
 
+// 대기 오염 항목에 대한 데이터 클래스
 data class DustItem(
     val so2Grade: String,
     val coFlag: String?,
