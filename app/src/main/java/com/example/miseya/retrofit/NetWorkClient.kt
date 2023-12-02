55e6ca81cabe2ec2f1a13b4a96b482af004034a9
Lee do yeon <simon3397@gmail.com>
2023-12-03 01:03:50 +0900

    [DOCS] 주석 추가

--- app/src/main/java/com/example/miseya/retrofit/NetWorkClient.kt
+++ app/src/main/java/com/example/miseya/retrofit/NetWorkClient.kt
@@ -10,17 +10,21 @@ import java.util.concurrent.TimeUnit
 
 object NetWorkClient {
 
+    // 공공데이터포털에서 발급받은 API 키
     private const val DUST_BASE_URL = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/"
 
-
+    // OkHttpClient를 생성하는 함수
     private fun createOkHttpClient(): OkHttpClient {
         val interceptor = HttpLoggingInterceptor()
 
+        // 디버그 모드인 경우 HTTP 로그 레벨을 BODY로 설정하여 디버깅 정보를 출력
+        // 릴리즈 모드인 경우 로깅을 비활성화
         if (BuildConfig.DEBUG)
             interceptor.level = HttpLoggingInterceptor.Level.BODY
         else
             interceptor.level = HttpLoggingInterceptor.Level.NONE
 
+        // OkHttpClient를 설정 및 반환
         return OkHttpClient.Builder()
             .connectTimeout(20, TimeUnit.SECONDS)
             .readTimeout(20, TimeUnit.SECONDS)
@@ -29,6 +33,7 @@ object NetWorkClient {
             .build()
     }
 
+    // Retrofit 객체를 생성
     private val dustRetrofit = Retrofit.Builder()
         .baseUrl(DUST_BASE_URL)
         .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setLenient().create()))
@@ -36,6 +41,7 @@ object NetWorkClient {
             createOkHttpClient()
         ).build()
 
+    // Retrofit 인터페이스 구현한 NetWorkInterface 객체를 생성
     val dustNetWork: NetWorkInterface = dustRetrofit.create(NetWorkInterface::class.java)
 
 }
--- app/src/main/java/com/example/miseya/retrofit/NetWorkInterface.kt
+++ app/src/main/java/com/example/miseya/retrofit/NetWorkInterface.kt
@@ -4,7 +4,10 @@ import Dust
 import retrofit2.http.GET
 import retrofit2.http.QueryMap
 
+// 네트워크 요청을 정의하는 인터페이스
 interface NetWorkInterface {
+
+    // 시도별 실시간 측정정보 조회를 위한 GET 요청
     @GET("getCtprvnRltmMesureDnsty") //시도별 실시간 측정정보 조회 주소
     suspend fun getDust(@QueryMap param: HashMap<String, String>): Dust
 }
