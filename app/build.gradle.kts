2e89eee94a4d4d225a4985546654d73e3f7c00d2
simon3397 <simon3397@gmail.com>
2024-08-09 16:17:50 +0900

    [COMPLETION]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,9 +32,6 @@ android {
                 "proguard-rules.pro")
                 buildConfigField("String", "API_KEY", "api_key")
         }
-        debug {
-            buildConfigField("String", "API_KEY", "\"api_key/\"")
-        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
@@ -63,8 +60,8 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
@@ -73,10 +70,9 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation("com.github.skydoves:powerspinner:1.2.7")
 
-    val composeBom = platform("androidx.compose:compose-bom:2024.04.01")
     val composeVersion = "1.6.8"
-    implementation("androidx.compose:compose-bom:2024.06.00")
-    androidTestImplementation("androidx.compose:compose-bom:2024.06.00")
+    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.06.00"))
 
     // material design
     implementation("androidx.compose.ui:ui:$composeVersion")
@@ -90,10 +86,10 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
 
     // Optional - Integration with activities
-    implementation ("androidx.activity:activity-compose:1.9.0")
+    implementation ("androidx.activity:activity-compose:1.9.1")
     // Optional - Integration with ViewModels
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
 
     // UI Tests
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
